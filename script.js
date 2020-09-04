83e4137ab450e65898a653d956da4a898e031348
programmertaha <programmer.taha.1y@gmail.com>
2020-09-04 12:47:49 +0300

    initial comments

---
+++ script.js
@@ -0,0 +1,17 @@
+/*
+
+% Each player chooses either rock, paper, or scissors
+% The items are compared, and whichever player 
+% chooses the more powerful item wins
+
+! Rock destroys scissors
+! Scissors cut paper
+! Paper covers rock
+! If there’s a tie, then the game ends in a draw
+
+todo Get the user’s choice.
+* Get the computer’s choice.
+* Compare the two choices and determine a winner
+* Start the program and display the results
+
+*/
